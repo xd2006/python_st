aece9cf64f8c532eec2db285ad1a2759d21ea05a
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-08-20 01:40:35 +0300

    conftest fix

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,22 +1,19 @@
+import pytest
+
 from model.group import Group
 
+testdata = [
+    Group(name="testname", header="testheader", footer="testfooter"),
+    Group(name="", header="", footer="")
+]
 
-def test_add_group(app):
+
+@pytest.mark.parametrize("group", testdata)
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     group = Group(name="testname", header="testheader", footer="testfooter")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.group_count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max)==sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.group_count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
