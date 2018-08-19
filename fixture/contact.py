86615bc2487fc068273db9daff86742a70043357
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-08-19 16:58:30 +0300

    new phones test

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,9 +1,9 @@
-from fixture.application import Application
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
-    def __init__(self, app: Application):
+    def __init__(self, app):
         self.app = app
 
     def add(self, contact):
@@ -107,5 +107,19 @@ class ContactHelper:
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(first_name=firstname, last_name=lastname, id=id, homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone   )
+        return Contact(first_name=firstname, last_name=lastname, id=id, homephone=homephone,
+                       mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
+
+    def get_contact_from_view_page(self, index) -> Contact:
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        secondaryphone = re.search("F: (.*)", text).group(1)
+        return Contact(homephone=homephone,
+                       mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
+
+
 
--- tests/test_phones.py
+++ tests/test_phones.py
@@ -10,6 +10,13 @@ def test_phones_on_home_page(app: Application):
     assert contact_from_home_page.workphone == clear(contact_from_edit_page.workphone)
     assert contact_from_home_page.mobilephone == clear(contact_from_edit_page.mobilephone)
 
+def test_phones_on_contact_view_page(app: Application):
+    contact_from_view_page: Contact = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page: Contact = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+
 def clear(s):
     return re.sub("([^\w^+])", "", s)
 
