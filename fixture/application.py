253a41e6703bb30b19039cb7c9d151124cecd8ca
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-08-20 05:01:58 +0300

    Command line params added

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 
 from fixture.contact import ContactHelper
 from fixture.general import GeneralHelper
@@ -7,23 +7,31 @@ from fixture.session import SessionHelper
 
 
 class Application:
-    wd: WebDriver
+    wd: webdriver
     general: GeneralHelper
     group: GroupHelper
     contact: ContactHelper
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False},
+    def __init__(self, browser, base_url):
+        if (browser=="firefox"):
+            self.wd = webdriver.Firefox(capabilities={"marionette": False},
                             firefox_binary="C:/Program Files (x86)/Mozilla Firefox ESR/firefox.exe")
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser &s" % browser)
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.general = GeneralHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
