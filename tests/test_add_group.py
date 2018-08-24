a199260092ba77a2deeba60d8dac81768b995e79
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-08-25 00:25:59 +0300

    Test data moved to separate file

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,28 +1,7 @@
-import re
-
 import pytest
-import random
-import string
 
 from model.group import Group
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10
-    str = prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    return clear(str.strip())
-
-
-def clear(s):
-    return re.sub("\s+", " ", s)
-
-
-testdata = [
-               Group(name=name, header=header, footer=footer)
-               for name in ["", random_string("testname", 10)]
-               for header in ["", random_string("testheader", 20)]
-               for footer in ["", random_string("testfooter", 20)]
-           ]
+from data.add_group import testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
