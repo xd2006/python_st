fce17a7e1d452c099e5ee3222f3293bf6ec14995
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-08-25 14:22:13 +0300

    jsonpickle used

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -14,8 +14,8 @@ def test_add_group(app, group):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_group_v(app, data_groups):
-    group = data_groups
+def test_add_group_v(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.group_count()
