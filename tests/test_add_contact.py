094588ae288030969558af9362cc1ea0556e2cf6
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-09-02 23:00:41 +0300

    Add contact and group tests modified

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -2,12 +2,11 @@
 from fixture.application import Application
 from model.contact import Contact
 
-def test_add_contact_v(app: Application, json_contacts):
+def test_add_contact(app: Application, db, json_contacts):
     contact = json_contacts
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     app.contact.add(contact)
-    assert len(old_contacts) + 1 == app.contact.contact_count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     old_contacts.append(contact)
 
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -5,20 +5,18 @@ from data.groups import testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
-    old_groups = app.group.get_group_list()
+def test_add_group(app, db, group):
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.group_count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_group_v(app, json_groups):
+def test_add_group_v(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.group_count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
